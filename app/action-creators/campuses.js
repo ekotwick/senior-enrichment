3413441867762398823db72ceb4a8ddcdfa319e5
ekotwick <edmond.kotwick@gmail.com>
2017-04-10 12:34:45 -0400

    action creators created

--- app/action-creators/campuses.js
+++ app/action-creators/campuses.js
@@ -6,8 +6,35 @@ import {
 } from '../constants';
 import axios from 'axios';
 
-export const create = campus => ({type: CREATE_CAMPUS, campus});
-export const update = campus => ({type: UPDATE_CAMPUS, campus});
-export const destory = campus => ({type: DELETE_CAMPUS, campus});
-export const set = campuses => ({type: SET_CAMPUSES, campuses});
+export const createC = campus => ({type: CREATE_CAMPUS, campus});
+export const updateC = campus => ({type: UPDATE_CAMPUS, campus});
+export const destroyC = campus => ({type: DELETE_CAMPUS, campus});
+export const setC = campuses => ({type: SET_CAMPUSES, campuses});
+
+export const createCampus = campus => dispatch => {
+	axios.post('/api/campuses', campus)
+		.then(res => dispatch(createC(res.data)))
+		.catch(err => console.error(`Creating campus: ${campus} unsuccessful`, err));
+};
+
+export const updateCampus = (id, campus) => dispatch => {
+	axios.put(`/api/campuses/:${id}`, campus)
+		.then(res => dispatch(updateC(res.data)))
+		.catch(err => console.error(`Updating campus: ${campus} unsuccessful`));
+};
+
+export const destroyCampus = id => dispatch => {
+	dispatch(destroyC(id));
+	axios.delete(`/api/campuses/${id}`)
+		.catch(err => console.error(`Destroying campus: ${campus} unsuccessful`));
+};
+
+export const getAllCampuses = () => dispatch => {
+		axios.get('/api/campuses')
+			.then(res => dispatch(setC(res.data)))
+			.catch(err => console.error('Retrieving campuses unsuccessful', err));
+};
+
+
+
 
--- app/action-creators/students.js
+++ app/action-creators/students.js
@@ -6,8 +6,31 @@ import {
 } from '../constants';
 import axios from 'axios';
 
-export const create = student => ({type: CREATE_STUDENT, student});
-export const update = student => ({type: UPDATE_STUDENT, student});
-export const destory = student => ({type: DELETE_STUDENT, student});
-export const set = students => ({type: SET_STUDENTS, students});
+export const creatS = student => ({type: CREATE_STUDENT, student});
+export const updatS = student => ({type: UPDATE_STUDENT, student});
+export const destorS = student => ({type: DELETE_STUDENT, student});
+export const seS = students => ({type: SET_STUDENTS, students});
 
+export const createStudent = student => dispatch => {
+	axios.post('/api/students', student)
+		.then(res => dispatch(createS(res.data)))
+		.catch(err => console.error(`Creating student: ${student} unsuccessful`, err));
+};
+
+export const updateStudent = (id, student) => dispatch => {
+	axios.put(`/api/students/:${id}`, student)
+		.then(res => dispatch(updateS(res.data)))
+		.catch(err => console.error(`Updating student: ${student} unsuccessful`));
+};
+
+export const destroyStudent = id => dispatch => {
+	dispatch(destroyS(id));
+	axios.delete(`/api/students/${id}`)
+		.catch(err => console.error(`Destroying student: ${student} unsuccessful`));
+};
+
+export const getAllStudents = () => dispatch => {
+		axios.get('/api/students')
+			.then(res => dispatch(setS(res.data)))
+			.catch(err => console.error('Retrieving students unsuccessful', err));
+};
